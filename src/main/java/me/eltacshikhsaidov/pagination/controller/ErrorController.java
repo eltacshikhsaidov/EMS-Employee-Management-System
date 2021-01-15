786e737eb8ba12af11a27d29c07a8b796b1c526c
eltacshikhsaidov <eltac.shixseyidov250301@gmail.com>
2021-01-16 02:10:06 +0400

    handling error page

---
+++ src/main/java/me/eltacshikhsaidov/pagination/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package me.eltacshikhsaidov.pagination.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error")
+    public String errorPage() {
+        return "error";
+    }
+}
